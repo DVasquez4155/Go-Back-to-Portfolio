3d6144b2d7c090bb23b8265e794ba367f40a9128
Daniel V <main@dvasquez4155.com>
2020-02-05 13:08:15 -0800

    Added Local Storage

---
+++ localStorage.js
@@ -0,0 +1,18 @@
+var values = getValues();
+function getValues(path) {
+    var retrievedObject = localStorage.getItem(path));
+    if (retrievedObject == null || retrievedObject == undefined) {
+        return [];
+    }
+    return JSON.parse(retrievedObject);
+}
+function updateValues(path) {
+    localStorage.setItem(path, JSON.stringify(values));
+}
+function addValue(value) {
+    if (values.indexOf(value) != -1) {
+        return;
+    }
+    values.push(value);
+    updateValues()
+}
