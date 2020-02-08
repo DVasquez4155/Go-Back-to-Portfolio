d6aa5e42482394160c8cf73641d28c6320fa66cc
Daniel V <main@dvasquez4155.com>
2020-02-08 10:06:09 -0800

    Commented usage

--- JSON.js
+++ JSON.js
@@ -1,16 +1,17 @@
 //Example of how this API can be used
-var weather = new getResponse("https://api.openweathermap.org/data/2.5/weather", "?appid=01ca9f9b934a40c31918c2ee0f5d52e4", "&q=San+Jose")
-var cat = new getResponse("https://aws.random.cat/meow", "", "")
-weather.done(
-    function (response){
-        console.log(response)
-    }
-)
-cat.done(
-    function (response){
-        console.log(response)
-    }
-)
+// var weather = new getResponse("https://api.openweathermap.org/data/2.5/weather", "?appid=01ca9f9b934a40c31918c2ee0f5d52e4", "&q=San+Jose")
+// var cat = new getResponse("https://aws.random.cat/meow", "", "")
+// weather.done(
+//     function (response){
+//         console.log(response)
+//     }
+// )
+// cat.done(
+//     function (response){
+//         console.log(response)
+//     }
+// )
+
 //Response
 function getResponse(url, apiKey, query) {
     this.url = url;
